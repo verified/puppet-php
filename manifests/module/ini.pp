61a183592f78d24d9c62693c1c9f56ff95c770fb
ekhaled <me.khaled@gmail.com>
2013-11-19 14:15:01 +0000

    Update ini.pp

--- manifests/module/ini.pp
+++ manifests/module/ini.pp
@@ -21,7 +21,7 @@ define php::module::ini (
     $ensure   = undef
 ) {
     
-    $zend = $isZend ? "zend_" : " "
+    $zend = ($isZend == true) ? "zend_" : ""
     
     # Strip 'pecl-*' prefix is present, since .ini files don't have it
     $modname = regsubst($title , '^pecl-', '', G)
